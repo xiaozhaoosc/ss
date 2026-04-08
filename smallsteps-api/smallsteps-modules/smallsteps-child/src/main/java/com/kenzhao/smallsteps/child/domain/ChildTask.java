6dc90d6f3fa4bdccf6aae4a90cc4904cc7acff71
xiaozhaoosc <1130473608@qq.com>
2026-04-08 17:16:28 +0000

    feat: 优化Child/Parent模块与App/UI/API功能
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

---
+++ smallsteps-api/smallsteps-modules/smallsteps-child/src/main/java/com/kenzhao/smallsteps/child/domain/ChildTask.java
@@ -0,0 +1,45 @@
+package compackage com.kenzhao.smallsteps.child.domain;
+
+import com.baomidou.mybatispackage com.kenzhao.smallsteps.child.domain;
+
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+package com.kenzhao.smallsteps.child.domain;
+
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.kenzhao.smallsteps.commonpackage com.kenzhao.smallsteps.child.domain;
+
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.kenzhao.smallsteps.common.core.domain.BaseEntity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**package com.kenzhao.smallsteps.child.domain;
+
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.kenzhao.smallsteps.common.core.domain.BaseEntity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 儿童任务执行记录
+ */
+@Data
+@EqualsAndHashCode(cpackage com.kenzhao.smallsteps.child.domain;
+
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.kenzhao.smallsteps.common.core.domain.BaseEntity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 儿童任务执行记录
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+@TableName("child_task")
+public class ChildTask
